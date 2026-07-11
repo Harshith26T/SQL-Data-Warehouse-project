5eda0655d413ede6d926a479d86ca62f7845147b
Harshith T <158144013+Harshith26T@users.noreply.github.com>
2026-07-11 14:25:11 +0530

    Update DDL.silver.sql

--- Scripts/silver/DDL.silver.sql
+++ Scripts/silver/DDL.silver.sql
@@ -29,14 +29,16 @@ DROP TABLE silver.crm_prd_info;
 GO
 CREATE TABLE silver.crm_prd_info (
     prd_id  INT,
+	cat_id NVARCHAR(50),
     prd_key NVARCHAR(50),
     prd_nm  NVARCHAR(50),
     prd_cost INT,
     prd_line  NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt DATETIME,
+    prd_start_dt DATE,
+    prd_end_dt DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
+
 
 if OBJECT_ID('silver.crm_sales_details', 'U') is not null
 DROP TABLE silver.crm_sales_details;
